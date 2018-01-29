28b8b7d9e0b9f83acf8fe813e707d525e5f170c9
Stephen Thurman <thurman024@gmail.com>
2018-01-29 17:58:39 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song
+  @@song_count = 0
+  attr_reader :name, :artist, :genre
+
+  def initialize(name)
+    @name = name
+    @@song_count += 1
+  end
+
+  def artist=(artist)
+    @artist = artist
+    all_artists << artist
+  end
+  def artists
+    
+end
+  
